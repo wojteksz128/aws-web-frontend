87947844e8266212ad47315177911e2573f80343
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2019-02-24 16:39:02 +0100

    Repare viewing of object lists

--- actions/listobject.js
+++ actions/listobject.js
@@ -12,36 +12,56 @@ var nazwy =[];
 var adresy =[];
 var message = 0;
 
-var task = function(request, callback){	
+var task = function(request, callback) {	
 	AWS.config.loadFromPath(AWS_CONFIG_FILE);
 	var S3 = new AWS.S3();
 
 	var params = {
-		      Bucket: 'psoir-test-bucket', /* required */
-			  Marker: 'images'
-			  
+		Bucket: 'psoir-test-bucket', /* required */
+		Prefix: 'images/'
 	};
-	fields = [];
-	nazwy = [];
-	adresy = [];
+	imageFiles = [];
+
 	S3.listObjects(params, function(err, data) {	
-		if (err) console.log(err, err.stack); // an error occurred
-		else{		 	
-			for(var i=0; i<data.Contents.length;i++){
-				fields[i] = data.Contents[i].Key;
-			}
-			for(var i=0; i<data.Contents.length;i++){
-				nazwy[i] = data.Contents[i].Key.substring(8);
-				S3.getSignedUrl('getObject', params={Bucket: 'psoir-test-bucket',Key:data.Contents[i].Key}, function (err, url) {
-					adresy[i]=url;
-				});
-			}
-			exports.Pola= fields;
-			exports.Nazwy = nazwy;
-			exports.Adresy = adresy;
-		
-			callback(null, {template: template, params:{fields:fields, bucket:"psoir-test-bucket",names:nazwy,adresy:adresy,message:message}});
+		if (err) {
+			console.log(err, err.stack);
+			return;
 		}
+
+		for(var i = 0; i < data.Contents.length; ++i) {
+			var object = data.Contents[i];
+			if (object.Size == 0) continue;		// This is directory
+			
+			var image = {
+				field: object.Key,
+				fileName: getFileName(object.Key),
+				url: S3.getSignedUrl('getObject', params={
+					Bucket: 'psoir-test-bucket',
+					Key: data.Contents[i].Key
+				})
+			};
+
+			imageFiles.push(image);
+		}
+		exports.Pola= fields;
+		exports.Nazwy = nazwy;
+		exports.Adresy = adresy;
+	
+		callback(null, {
+			template: template, 
+			params: {
+				fields: fields, 
+				bucket: "psoir-test-bucket",
+				imageFiles: imageFiles,
+				message: message
+			}
+		});
 	});
 }
+
+function getFileName(path) {
+	var parts = path.split('/');
+	return parts[parts.length - 1];
+}
+
 exports.action = task;
